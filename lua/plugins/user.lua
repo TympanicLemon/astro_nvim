8b8e19527ac7881e86cb73b25a46b8ef068438d2
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-03-06 12:03:46 -0800

    added vim-tmux nav

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,4 +9,14 @@ return {
       vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle)
     end,
   },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
+  },
 }
