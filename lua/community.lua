41fa8bcb2b2e98c10eeeac9892e1728d282c26fc
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-03-08 01:53:04 -0800

    added oil, heirline-vscode, and nvim-spectre

--- lua/community.lua
+++ lua/community.lua
@@ -4,4 +4,7 @@ return {
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
 }
