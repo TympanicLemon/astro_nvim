bdc4c29fe7951a464bafc2829bbca168cb86e8eb
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-03-08 02:37:29 -0800

    got rid of override.lua, cant get it to work for nvim-jdtls for now

--- lua/plugins/override.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mfussenegger/nvim-jdtls",
-  config = function(_, opts)
-    opts.settings.java.implementationsCodeLens.enabled = false
-    opts.settings.java.referencesCodeLens.enabled = false
-    opts.settings.signatureHelp.enabled = false
-  end,
-}
