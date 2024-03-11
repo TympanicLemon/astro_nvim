558ffe60aca8c8f72c39ad5975af85b1e2197291
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-03-10 18:44:27 -0700

    got rid of oil and java stuff

--- lua/community.lua
+++ lua/community.lua
@@ -2,10 +2,8 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
-  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
 }
