b83d1f253fb6bbd3db71c8a54e43414a90f79547
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-16 20:30:55 -0700

    got rid of everything language related, will use this for now as my text editor but not for coding projects

--- lua/community.lua
+++ lua/community.lua
@@ -2,8 +2,5 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
-  { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
