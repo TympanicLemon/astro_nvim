fb9a7ebd993a4db94d4b097073141acb428f1d9f
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-16 20:31:56 -0700

    got rid of hack and asm from ts, if needed just install manually

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,11 +3,9 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "asm",
       "bash",
       "c",
       "diff",
-      "hack",
       "html",
       "java",
       "javascript",
