d39d3252d65fa651b5aa3f3b72cd713f14bb5990
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-17 11:45:12 -0700

    added web stuff back in

--- lua/community.lua
+++ lua/community.lua
@@ -2,5 +2,8 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
