4c3d7bd29637083f7e548e004aecd3d8f5847190
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-17 23:39:22 -0700

    added python support, mainly to do discord bot stuff

--- lua/community.lua
+++ lua/community.lua
@@ -4,6 +4,7 @@ return {
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.astro" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
