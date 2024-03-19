09ec864753986e64692a9a8f7079002890045b34
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-19 08:09:40 -0700

    got rid of python and web dev stuff

--- lua/community.lua
+++ lua/community.lua
@@ -2,10 +2,6 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
-  { import = "astrocommunity.pack.astro" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
