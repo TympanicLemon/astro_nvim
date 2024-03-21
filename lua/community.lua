67aee8ab1d8eb731c3d4d78120c4a6c628908590
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-03-21 00:16:47 -0700

    testing out rust pack

--- lua/community.lua
+++ lua/community.lua
@@ -2,5 +2,6 @@
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
